34a95c15c89d0b45dab788b6dda9619b6cc13902
Gaston Galdi <gaston.hares@gmail.com>
2023-06-10 05:24:01 -0300

    modificacion para javascript

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const numero = prompt("Ingresa un número:");
+
+// Verificamos si el número ingresado es positivo, negativo o cero
+if (numero > 0) {
+  console.log("El número ingresado es positivo.");
+} else if (numero < 0) {
+  console.log("El número ingresado es negativo.");
+} else {
+  console.log("El número ingresado es cero.");
+}
